ac4188127d1ce1014907ed0b5752d567355a095f
adrianojlt <adrianojlt@gmail.com>
2026-03-01 10:39:36 +0000

    first commit - base project

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Clipboard Manager</h1>
+    </main>
+  );
+}
+
+export default App;
